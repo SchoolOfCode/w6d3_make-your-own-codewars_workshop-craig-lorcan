77c81e58c4b1ce4b6396a0e46d7599672fb14307
Lorcan Mc <lorcan.jmc@gmail.com>
2021-12-22 16:14:28 +0000

    with solution code

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 /* 
-👉 Write your kata here!
-Write your kata's scenario in a comment in [main.js](main.js), 
-and include the start of the function that CodeWarriors will use to solve it 
-(like you've seen on the katas you've done before).
 
-Wriet a function to calculates the the BMI of a person.  
+Given a persons weight and height, write a function to calculates their BMI.
 
-if bmi < 18.5 return "Underweight"
+Google *BMI equation* to find the formula.
 
-if bmi < 25.0 return "Normal"
+if bmi <= 18.5 return "Your weight is underweight"
 
-if bmi < 30.0 return "Overweight"
+if bmi <= 25.0 return "Your weight is normal"
 
-if bmi > 30 return "Obese"
+if bmi <= 30.0 return "Your weight is overweight"
+
+if bmi > 30 return "Your weight is obese"
 
 
 */
@@ -20,10 +18,11 @@ if bmi > 30 return "Obese"
 //👉 Write the function your CodeWarriors will start with below here:
 
 export function  bmiChecker(weight, height) {
-    const bmi = (weight/height**2)
+    
+    const bmi = (weight/height**2);
 
-    if (bmi <= 18.5) {return "Underweight"}
-    if (bmi <= 25.0) {return "Normal"}
-    if (bmi <= 30.0) {return "Overweight"}
-    if (bmi > 30) {return "Obese"}
+    if (bmi <= 18.5) {return "Your weight is underweight"}
+    if (bmi <= 25.0) {return "Your weight is normal"}
+    if (bmi <= 30.0) {return "Your weight is overweight"}
+    if (bmi > 30) {return "Your weight is obese"}
 }
--- main.test.js
+++ main.test.js
@@ -4,8 +4,14 @@ import {bmiChecker} from "./main.js"
 
 describe("Testing to see if the bmi function returns Underweight", function(){
     test("Basic bmi test to return Underweight", () => {
-        const expected = "Underweight"
+        const expected = "Your weight is underweight"
         const actual = bmiChecker(50, 1.8)
+    
+        expect(actual).toEqual(expected)
+    })
+    test("Basic bmi test to return Underweight", () => {
+        const expected = "Your weight is underweight"
+        const actual = bmiChecker(45, 1.75)
     
         expect(actual).toEqual(expected)
     })
@@ -14,7 +20,7 @@ describe("Testing to see if the bmi function returns Underweight", function(){
 
 describe("Testing to see if the bmi function returns normal", function(){
     test("Basic bmi test to return Normal", () => {
-        const expected = "Normal"
+        const expected = "Your weight is normal"
         const actual = bmiChecker(80, 1.80)
     
         expect(actual).toEqual(expected)
@@ -23,7 +29,7 @@ describe("Testing to see if the bmi function returns normal", function(){
 
 describe("Testing to see if the bmi function returns Overweight", function(){
     test("Basic bmi test to return Overweight", () => {
-        const expected = "Overweight"
+        const expected = "Your weight is overweight"
         const actual = bmiChecker(90, 1.8)
     
         expect(actual).toEqual(expected)
@@ -33,8 +39,14 @@ describe("Testing to see if the bmi function returns Overweight", function(){
 
 describe("Testing to see if the bmi function returns Obese", function(){
     test("Basic bmi test to return Obese", () => {
-        const expected = "Obese"
+        const expected = "Your weight is obese"
         const actual = bmiChecker(100, 1.8)
+    
+        expect(actual).toEqual(expected)
+    })
+    test("Basic bmi test to return Obese", () => {
+        const expected = "Your weight is obese"
+        const actual = bmiChecker(120, 1.9)
     
         expect(actual).toEqual(expected)
     })
