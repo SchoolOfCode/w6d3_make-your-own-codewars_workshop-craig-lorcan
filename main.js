a6942ca03c115a5d5556bcbc7929f033db1a7a0d
Lorcan Mc <lorcan.jmc@gmail.com>
2021-12-22 16:42:51 +0000

    tweaked insrtuctions

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ Given a persons weight and height, write a function to calculates their BMI.
 
 Google *BMI equation* to find the formula.
 
-if bmi <= 18.5 return "Your weight is underweight"
+if bmi is less than or equal to 18.5 return "Your weight is underweight"
 
-if bmi <= 25.0 return "Your weight is normal"
+if bmi is less than or equal to 25.0 return "Your weight is normal"
 
-if bmi <= 30.0 return "Your weight is overweight"
+if bmi is less than or equal to 30.0 return "Your weight is overweight"
 
-if bmi > 30 return "Your weight is obese"
+if bmi is greater than 30 return "Your weight is obese"
 
 
 */
