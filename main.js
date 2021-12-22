788880a82f34eb20f1709408e31e7870f2f0e139
Craig <cbmaruziva@hotmail.co.uk>
2021-12-22 15:30:10 +0000

    step one done

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 /* 
 👉 Write your kata here!
+Write your kata's scenario in a comment in [main.js](main.js), 
+and include the start of the function that CodeWarriors will use to solve it 
+(like you've seen on the katas you've done before).
+
+Wriet a function to calculates the the BMI of a person.  
+
+if bmi < 18.5 return "Underweight"
+
+if bmi < 25.0 return "Normal"
+
+if bmi < 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function  bmiChecker(weight, height) {
+
+}
